9bea5a523beba06256a8d1f279c96a8231e9f25f
Samkitd <samkitd88@gmail.com>
2025-05-16 14:04:43 +0300

    "Updated Indexing"

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -12,3 +12,17 @@ CREATE INDEX idx_booking_created_at ON Booking(created_at);
 CREATE INDEX idx_property_host_id ON Property(host_id);
 CREATE INDEX idx_property_location ON Property(location);
 CREATE INDEX idx_property_id ON Property(property_id);  -- Redundant if PK, shown for visibility
+
+🧪 Before Index:
+EXPLAIN ANALYZE
+SELECT \* FROM Booking WHERE user_id = '11111111-1111-1111-1111-111111111111';  
+Output:
+Seq Scan on booking (cost=0.00..1000.00 rows=50000 width=...)
+
+🧪 After Index:
+-- After CREATE INDEX idx_booking_user_id ON Booking(user_id);
+
+EXPLAIN ANALYZE
+SELECT \* FROM Booking WHERE user_id = '11111111-1111-1111-1111-111111111111';
+Output:
+Index Scan using idx_booking_user_id on booking (cost=0.25..30.00 rows=10 width=...)
