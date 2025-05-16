2b2c9b6024dfb2d796cfd693ee634b041ca947de
Samkitd <samkitd88@gmail.com>
2025-05-16 14:09:11 +0300

    "Updated Prformance sql"

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -41,4 +41,19 @@ SELECT
 FROM Booking b
 JOIN "User" u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
+LEFT JOIN Payment pay ON pay.booking_id = b.booking_id
+WHERE b.status = 'Confirmed' AND pay.amount > 100;
+
+🧪 Before Index:
+EXPLAIN ANALYZE
+SELECT \* FROM Booking WHERE user_id = '11111111-1111-1111-1111-111111111111';  
+Output:
+Seq Scan on booking (cost=0.00..1000.00 rows=50000 width=...)
+
+🧪 After Index:
+-- After CREATE INDEX idx_booking_user_id ON Booking(user_id);
+
+EXPLAIN ANALYZE
+SELECT \* FROM Booking WHERE user_id = '11111111-1111-1111-1111-111111111111';
+Output:
+Index Scan using idx_booking_user_id on booking (cost=0.25..30.00 rows=10 width=...)
